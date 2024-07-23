a227c6f898eac5f471cc34863c4be419131366a6
ShivaSingh17121997 <shivasingh001997@gmail.com>
2024-07-23 09:17:32 +0530

    added

---
+++ firt-app/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>This is my first react app</h1>
+    </div>
+  );
+}
+
+export default App;
+
+
+// advantage of react , diff between framework and library
