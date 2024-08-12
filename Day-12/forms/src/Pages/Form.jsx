0c093a7b9092f89044d0e02b95027a4673ca8168
ShivaSingh17121997 <shivasingh001997@gmail.com>
2024-08-12 09:47:19 +0530

    added the comments in teh form component

--- Day-12/forms/src/Pages/Form.jsx
+++ Day-12/forms/src/Pages/Form.jsx
@@ -1,9 +1,10 @@
 import { useState } from "react";
 import "./Form.css"
 
-
-
+// comments are simple texts for poviding instructins and to disable some part of code
+// created a form component
 function Form() {
+    // use the useState hook for managingn the state
     const [name, setName] = useState('');
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
@@ -11,12 +12,16 @@ function Form() {
     const [gender, setGender] = useState('');
     const [errMsg, setErrMsg] = useState({})
 
+
+    // consoling the error msg
     console.log(errMsg)
 
 
+    // handeling form data with validation
     const handleForm = (e) => {
         e.preventDefault();
 
+        // empty err obj for storing err msg
         let errObj = {};
 
         if (!name) {
@@ -27,12 +32,15 @@ function Form() {
             errObj.password = "password is required"
         }
 
+        // if keys length of  err msg  is greater than 0 only then data is added in errobj
         if (Object.keys(errObj).length > 0) {
             setErrMsg(errObj)
 
         } else {
+            // consoling the submitted data
             console.log(name, email, password, married, gender)
 
+            // emptying the all input fields
             setName('');
             setEmail('');
             setPassword('');
@@ -47,8 +55,7 @@ function Form() {
     return <div className="main-Container">
         <h1>Form</h1>
 
-
-
+        {/* created form tag  */}
         <form className="form-container" onSubmit={handleForm} >
 
             <input type="text" placeholder="enter your name" value={name} onChange={(e) => setName(e.target.value)} />
@@ -65,12 +72,15 @@ function Form() {
             {errMsg.password && <span style={{ color: "red" }} >{errMsg.password}</span>}
             <br /><br />
 
+
+            {/* check box  */}
             <label >
                 <input type="checkbox" checked={married} onChange={(e) => setMarried(!married)} />
                 Married
 
             </label> <br /> <br />
 
+            {/* select input field */}
             <select onChange={(e) => setGender(e.target.value)}  >
                 <option value="">Gender</option>
                 <option value="male">Male</option>
