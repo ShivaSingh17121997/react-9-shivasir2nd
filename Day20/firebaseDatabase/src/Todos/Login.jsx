68465860d2e9aa4de68ebb134682bedc31904db7
ShivaSingh17121997 <shivasingh001997@gmail.com>
2024-10-22 09:33:01 +0530

    this is my firebase project of react 9 am class

--- Day20/firebaseDatabase/src/Todos/Login.jsx
+++ Day20/firebaseDatabase/src/Todos/Login.jsx
@@ -1,7 +1,103 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { getAuth, signInWithEmailAndPassword, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+
+const provider = new GoogleAuthProvider()
 
 export default function Login() {
+    const [userEmail, setUseEmail] = useState("");
+    const [password, setPasswrod] = useState("");
+
+
+    const handleLogin = (e) => {
+        e.preventDefault();
+
+        // validation logic
+        if (!userEmail || !password) {
+            alert("all fiends required")
+            return;
+        }
+
+        // login logic form firebase 
+        const auth = getAuth();
+        signInWithEmailAndPassword(auth, userEmail, password)
+            .then((userCredential) => {
+                // Signed in 
+                const user = userCredential.user;
+                console.log(user.accessToken)
+                // ...
+            })
+            .catch((error) => {
+                const errorCode = error.code;
+                const errorMessage = error.message;
+                console.log(errorCode)
+                console.log(errorMessage)
+            });
+
+    }
+
+    // signinwith google pop up
+
+    const handleSigninWithGoogle = () => {
+
+        const auth = getAuth();
+        signInWithPopup(auth, provider)
+            .then((result) => {
+                // This gives you a Google Access Token. You can use it to access the Google API.
+                const credential = GoogleAuthProvider.credentialFromResult(result);
+                console.log(credential)
+                const token = credential.accessToken;
+                console.log(token)
+                // The signed-in user info.
+                const user = result.user;
+                console.log(user)
+                // IdP data available using getAdditionalUserInfo(result)
+                // ...
+            }).catch((error) => {
+                // Handle Errors here.
+                const errorCode = error.code;
+                const errorMessage = error.message;
+                console.log(errorCode)
+                console.log(errorMessage)
+                // The email of the user's account used.
+                const email = error.customData.email;
+                console.log(email)
+                // The AuthCredential type that was used.
+                const credential = GoogleAuthProvider.credentialFromError(error);
+                console.log(credential)
+                // ...
+            });
+
+    }
+
+
+
+
     return (
-        <div>Login</div>
+        <div>
+            <h1>Login</h1>
+
+            <div>
+                <form onSubmit={handleLogin} >
+                    <div>
+                        <input value={userEmail} onChange={(e) => setUseEmail(e.target.value)} type="text" placeholder='Email' />
+                    </div>
+                    <br />
+                    <div>
+                        <input value={password} onChange={(e) => setPasswrod(e.target.value)} type="password" placeholder='Enter your password' />
+                    </div>
+                    <br />
+                    <div>
+                        <button>Login</button>
+                    </div>
+                </form>
+            </div>
+            <br /><br />
+
+            <div>
+                <button onClick={handleSigninWithGoogle} >Singn in with google</button>
+            </div>
+
+
+        </div>
     )
 }
--- Day20/firebaseDatabase/src/Todos/Register.jsx
+++ Day20/firebaseDatabase/src/Todos/Register.jsx
@@ -1,7 +1,62 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
 
 export default function Register() {
+  const [userEmail, setUseEmail] = useState("");
+  const [password, setPasswrod] = useState("");
+
+
+  const handleRegister = (e) => {
+    e.preventDefault();
+
+    // validation logic
+    if (!userEmail || !password) {
+      alert("all fiends required")
+      return;
+    }
+
+    // auth logic
+    const auth = getAuth();
+    createUserWithEmailAndPassword(auth, userEmail, password)
+      .then((userCredential) => {
+        // Signed up 
+        const user = userCredential.user;
+        console.log(user)
+        alert("user registered successfully")
+        // ...
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.log(errorCode)
+        console.log(errorMessage)
+        // ..
+      });
+
+
+  }
+
   return (
-    <div>Register</div>
+    <div>
+      <h1>Register</h1>
+
+      <div>
+        <form onSubmit={handleRegister} >
+          <div>
+            <input value={userEmail} onChange={(e) => setUseEmail(e.target.value)} type="text" placeholder='Email' />
+          </div>
+          <br />
+          <div>
+            <input value={password} onChange={(e) => setPasswrod(e.target.value)} type="password" placeholder='Enter your password' />
+          </div>
+          <br />
+          <div>
+            <button>Register</button>
+          </div>
+        </form>
+      </div>
+
+
+    </div>
   )
 }
